a4073b4fcc2bb0ae24ac9d09b4c53549fe5ac13e
MuriloRoveri <RM97893@fiap.com.br>
2023-04-18 10:47:46 -0300

    Convertendo HTMLCollection em array

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,5 +1,4 @@
-const liElement = document.getElementsByTagName("li");
-
+// const liElement = document.getElementsByTagName("li");
 
 // for (let index = 0; index < liElement.length ; index++) {
 //     console.log(liElement[index].textContent)
@@ -8,13 +7,13 @@ const liElement = document.getElementsByTagName("li");
 // console.log(liElement.length);
 
 //Declaração de Arrays
-let nr1 = [1,2,3,4,5];
-let nr2 = [6,7,8,9,10];
-let nr3 = [nr1,nr2]
+// let nr1 = [1,2,3,4,5];
+// let nr2 = [6,7,8,9,10];
+// let nr3 = [nr1,nr2]
 
-console.log(`Array-1 ${nr1}`);
-console.log(`Array-2 ${nr2}`);
-console.log(`Array-3 ${nr3}`);
+// console.log(`Array-1 ${nr1}`);
+// console.log(`Array-2 ${nr2}`);
+// console.log(`Array-3 ${nr3}`);
 
 //Listando um array! Técnica Especial!
 // nr3.forEach((ItemDoArray)=>{
@@ -22,9 +21,31 @@ console.log(`Array-3 ${nr3}`);
 // })
 
 // Listando o Array com MAP
+// nr3.map((numero,key)=>{
+//     console.log(`${key+1} - Itens do array - ${numero}`);
+//     numero.forEach((digitos)=>{
+//         console.log(digitos)
+//     })
+// })
+
+//Declaração de Arrays para uso do SPREAD
+let nr1 = [1,2,3,4,5];
+let nr2 = [6,7,8,9,10];
+// Espalhar o array com SPREAD [ ... ]
+let nr3 = [...nr1,...nr2]
+
+console.log(`Array-1 ${nr1}`);
+console.log(`Array-2 ${nr2}`);
+console.log(`Array-3 ${nr3}`);
+
 nr3.map((numero,key)=>{
-    console.log(`${key+1} - Itens do array - ${numero}`);
-    numero.forEach((digitos)=>{
-        console.log(digitos)
-    })
+     console.log(`${key+1} - Itens do array - ${numero}`);
+})
+
+// Convertendo HTMLCollection em Array de forma indireta
+const liElementHtml = document.getElementsByTagName("li");
+const liElementArray = [...liElementHtml];
+
+liElementArray.map((item,key)=>{
+    console.log(`${key+1} - Itens do li - ${item.textContent}`);
 })
